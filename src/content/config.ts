8aea913129baa99857943db7ad7bf48bacc000b9
CosmoLau <cosmolau@qq.com>
2025-12-31 15:51:03 +0800

    feat: 更换 Fuwari 主题模板

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,30 +1,28 @@
-import {defineCollection, z} from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional().nullable(),
-    date: z.date(),
-    tags: z.array(z.string()).or(z.string()).optional().nullable(),
-    category: z.array(z.string()).or(z.string()).default('uncategorized').nullable(),
-    sticky: z.number().default(0).nullable(),
-    mathjax: z.boolean().default(false).nullable(),
-    mermaid: z.boolean().default(false).nullable(),
-    draft: z.boolean().default(false).nullable(),
-    toc: z.boolean().default(true).nullable(),
-    donate: z.boolean().default(true).nullable(),
-    comment: z.boolean().default(true).nullable(),
-    ogImage: z.string().optional()
-  }),
+const postsCollection = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		published: z.date(),
+		updated: z.date().optional(),
+		draft: z.boolean().optional().default(false),
+		description: z.string().optional().default(""),
+		image: z.string().optional().default(""),
+		tags: z.array(z.string()).optional().default([]),
+		category: z.string().optional().nullable().default(""),
+		lang: z.string().optional().default(""),
+
+		/* For internal use */
+		prevTitle: z.string().default(""),
+		prevSlug: z.string().default(""),
+		nextTitle: z.string().default(""),
+		nextSlug: z.string().default(""),
+	}),
 });
-
-const feed = defineCollection({
-  schema: z.object({
-    date: z.date().or(z.string()).optional().nullable(),
-    donate: z.boolean().default(true),
-    comment: z.boolean().default(true),
-  })
-})
-
-export const collections = {blog, feed};
+const specCollection = defineCollection({
+	schema: z.object({}),
+});
+export const collections = {
+	posts: postsCollection,
+	spec: specCollection,
+};
